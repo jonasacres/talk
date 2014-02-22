428695859272f25d05c290f1e0086bd9bdd5ad3b
Jonas Acres <acresjonas@gmail.com>
2014-02-22 15:45:40 -0800

    Renamed talk.rb to talk

--- bin/talk.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-basepath = File.expand_path(File.dirname(__FILE__) + '/..')
-lib = File.join(basepath, 'lib')
-$LOAD_PATH.unshift(lib) if File.directory?(lib) && !$LOAD_PATH.include?(lib)
-
-require 'talk'
